d09a8163f6900e9980870e8d45eecf7b59cce0b0
shvetsov <dozakus@yandex.ru>
2021-09-26 17:11:45 +0300

    Update Homework

---
+++ 4.2.py
@@ -0,0 +1,16 @@
+!/usr/bin/env python3
+
+import os
+import sys
+
+#Проверка входящего параметра, отрицательно - проверяем текущую папку
+a = os.getcwd()
+if len(sys.argv)>=2:
+    a = sys.argv[1]
+bash_command = ["cd "+a, "git status 2>&1"]
+result_os = os.popen(' && '.join(bash_command)).read()
+for result in result_os.split('\n'):
+    if result.find('modified') != -1:
+        prepare_result = result.replace('\tmodified: ', '')
+        output=a+prepare_result
+	print(output)
